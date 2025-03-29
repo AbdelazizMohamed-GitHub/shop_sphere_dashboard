d1885b32cf319d5862b20990eb35e533cdd396b3
Abdelaziz Mohamed Abdelaziz <abdelazizmohameddewedar@gmail.com>
2025-03-29 13:06:27 +0200

    Finish Customer Screen

--- lib/features/presention/view/screen/customer_screen.dart
+++ lib/features/presention/view/screen/customer_screen.dart
@@ -5,6 +5,30 @@ class CustomerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Customers")),
+      body: ListView.builder(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        itemCount: 5,
+
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: ListTile(
+              title: Text("Customer $index"),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.message_rounded),
+              ),
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+
+              ),
+              subtitle: Text("Phone $index"),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
